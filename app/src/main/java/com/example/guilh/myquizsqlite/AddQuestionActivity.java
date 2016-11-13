84e737e675912518f712b76d7274ccb1f4edf858
Guilherme Agunzo <g.agunzo@gmail.com>
2016-11-13 15:55:48 -0200

    Logica de adicionar questões

--- app/src/main/java/com/example/guilh/myquizsqlite/AddQuestionActivity.java
+++ app/src/main/java/com/example/guilh/myquizsqlite/AddQuestionActivity.java
@@ -2,6 +2,11 @@ package com.example.guilh.myquizsqlite;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.guilh.db.DbHelper;
 
 public class AddQuestionActivity extends AppCompatActivity {
 
@@ -9,5 +14,36 @@ public class AddQuestionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_question);
+
+        Button btn = (Button) findViewById(R.id.addButton);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addQuestion();
+            }
+        });
+    }
+
+    private void addQuestion(){
+        EditText quesEd = (EditText) findViewById(R.id.QuesEditText);
+        EditText answerEd = (EditText) findViewById(R.id.answerEditText);
+        EditText optaEd = (EditText) findViewById(R.id.optaEditText);
+        EditText optbEd = (EditText) findViewById(R.id.optbEditText);
+        EditText optcEd = (EditText) findViewById(R.id.optcEditText);
+
+        String question,answer,opta,optb,optc;
+        DbHelper db= new DbHelper(this);
+
+        question = quesEd.getText().toString();
+        answer = answerEd.getText().toString();
+        opta = optaEd.getText().toString();
+        optb = optbEd.getText().toString();
+        optc = optcEd.getText().toString();
+
+        Question quest = new Question(question,opta,optb,optc,answer);
+
+        db.addQuestion(quest);
+
     }
 }
